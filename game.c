0fc3523253aa81b083ec5ecc5425e00c91ce2cf9
Elective <rodericparson@gmail.com>
2021-07-05 15:40:49 -0400

    bullet collision v1

--- game.c
+++ game.c
@@ -1,5 +1,53 @@
 #include <gbalib.h>
 
 int main() {
-    // Add code here!
+    // Sizes, Positions, Colors, Integers
+    int randomY = randomInteger(0, 159);
+    Position bulletRespawn = {250, randomY};
+
+    //Backgrounds, Sprites, Gifs
+
+    //button stuff
+
+    //Collisions
+    bool bulletCollision1 = checkCollisionSprite();
+    if (bulletCollision1 == true) {
+        hideSprite();
+        wait(1);
+        showSprite();
+        updatePosition(, bulletRespawn);
+        updateScreen();
+    }
+    bool bulletCollision2 = checkCollisionSprite();
+    if (bulletCollision2 == true) {
+        hideSprite();
+        wait(1);
+        showSprite();
+        updatePosition(, bulletRespawn);
+        updateScreen();
+    }
+    bool bulletCollision3 = checkCollisionSprite();
+    if (bulletCollision3 == true) {
+        hideSprite();
+        wait(1);
+        showSprite();
+        updatePosition(, bulletRespawn);
+        updateScreen();
+    }
+    bool bulletCollision4 = checkCollisionSprite();
+    if (bulletCollision4 == true) {
+        hideSprite();
+        wait(1);
+        showSprite();
+        updatePosition(, bulletRespawn);
+        updateScreen();
+    }
+    bool bulletCollision5 = checkCollisionSprite();
+    if (bulletCollision5 == true) {
+        hideSprite();
+        wait(1);
+        showSprite();
+        updatePosition(, bulletRespawn);
+        updateScreen();
+    }
 }
