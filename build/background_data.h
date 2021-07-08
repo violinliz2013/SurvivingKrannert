a42a72c1f86961fcf5406b84bb2f05bc26495eaf
violinliz2013 <violinliz2013@gmail.com>
2021-07-08 14:47:54 -0400

    lowercase_screennames

---
+++ build/background_data.h
@@ -0,0 +1,51 @@
+/*
+ * Exported with nin10kit v1.8
+ * Invocation command was nin10kit --mode=3 ./build/background_data ./backgrounds/titleScreen.png ./backgrounds/pauseScreen.png ./backgrounds/KrannertHall.png ./backgrounds/loseScreen.png ./backgrounds/winScreen.png 
+ * Time-stamp: Thursday 07/08/2021, 14:38:00
+ * 
+ * Image Information
+ * -----------------
+ * ./backgrounds/titleScreen.png 240@160
+ * ./backgrounds/pauseScreen.png 240@160
+ * ./backgrounds/KrannertHall.png 240@160
+ * ./backgrounds/loseScreen.png 240@160
+ * ./backgrounds/winScreen.png 240@160
+ * 
+ * All bug reports / feature requests are to be filed here https://github.com/TricksterGuy/nin10kit/issues
+ */
+
+#ifndef BACKGROUND_DATA_H
+#define BACKGROUND_DATA_H
+
+extern const unsigned short titleScreen[38400];
+#define TITLESCREEN_SIZE 76800
+#define TITLESCREEN_LENGTH 38400
+#define TITLESCREEN_WIDTH 240
+#define TITLESCREEN_HEIGHT 160
+
+extern const unsigned short pauseScreen[38400];
+#define PAUSESCREEN_SIZE 76800
+#define PAUSESCREEN_LENGTH 38400
+#define PAUSESCREEN_WIDTH 240
+#define PAUSESCREEN_HEIGHT 160
+
+extern const unsigned short KrannertHall[38400];
+#define KRANNERTHALL_SIZE 76800
+#define KRANNERTHALL_LENGTH 38400
+#define KRANNERTHALL_WIDTH 240
+#define KRANNERTHALL_HEIGHT 160
+
+extern const unsigned short loseScreen[38400];
+#define LOSESCREEN_SIZE 76800
+#define LOSESCREEN_LENGTH 38400
+#define LOSESCREEN_WIDTH 240
+#define LOSESCREEN_HEIGHT 160
+
+extern const unsigned short winScreen[38400];
+#define WINSCREEN_SIZE 76800
+#define WINSCREEN_LENGTH 38400
+#define WINSCREEN_WIDTH 240
+#define WINSCREEN_HEIGHT 160
+
+#endif
+
